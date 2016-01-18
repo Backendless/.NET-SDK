da47ece945c6d3a6432af30ce24f220ba1bc6b78
mark@themidnightcoders.com <mark@themidnightcoders.com>
2016-01-17 18:42:38 -0800

    removed test APP ID and API KEY

--- Samples/DataService/ToDoDemo/Defaults.cs
+++ Samples/DataService/ToDoDemo/Defaults.cs
@@ -5,10 +5,10 @@ using System.Text;
 
 namespace Examples.MessagingService.ToDoDemo
 {
-  internal static class Defaults
-  {
-    public const string APPLICATION_ID = "C3D3036A-BCDA-FFCF-FF9A-D1B496CA5900";
-    public const string SECRET_KEY = "E2DFE54D-2687-63AD-FFF3-D5B891142300";
-    public const string VERSION = "v1";
-  }
+    internal static class Defaults
+    {
+        public const string APPLICATION_ID = "";
+        public const string SECRET_KEY = "";
+        public const string VERSION = "v1";
+    }
 }
