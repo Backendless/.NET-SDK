5c2f5acfd249a1835216f86d4bb76e5bc0fbc131
Nikita <nikita@themidnightcoders.com>
2020-03-17 15:17:04 +0200

    Added classes Geometry and GeometryDTO

---
+++ Projects/Backendless.NET.Framework4.7/Geometry.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendlessAPI
+{
+  abstract class Geometry<T>
+  {
+    abstract public String GetGeoJSONType();
+
+    abstract public String GetWKTType();
+
+    abstract internal String JSONCoordinatePairs();
+
+    abstract internal String WKTCoordinatePairs();
+
+    public String AsGeoJSON()
+    {
+      return "{\"type\":\"" + this.GetGeoJSONType() + "\",\"coordinates:\":" + this.JSONCoordinatePairs() + "}";
+    }
+
+    public String AsWKT()
+    {
+      return GetWKTType() + $"({this.WKTCoordinatePairs()})";
+    }
+
+    public override string ToString()
+    {
+      return $"'{AsWKT()}'";
+    }
+  }
+}
---
+++ Projects/Backendless.NET.Framework4.7/GeometryDTO.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendlessAPI
+{
+  public class GeometryDTO
+  {
+    private String geomClass;
+    private int srsId;
+    private String geoJSON;
+
+    public String GeomClass
+    {
+      get { return geomClass; }
+      set { geomClass = value; }
+    }
+
+    public int SrsId
+    {
+      get { return srsId; }
+      set { srsId = value; }
+    }
+
+    public String GeoJSON
+    {
+      get { return geoJSON; }
+      set { geoJSON = value; }
+    }
+    public GeometryDTO()
+    {
+    }
+
+    public GeometryDTO(String geomClass, int srsId, String geoJSON)
+    {
+      this.geomClass = geomClass;
+      this.srsId = srsId;
+      this.geoJSON = geoJSON;
+    }
+
+    //public T Geometry where T : Geometry()
+    public override bool Equals( object obj )
+    {
+      if ( this == obj )
+        return true;
+      if ( !( obj is GeometryDTO ) )
+        return false;
+      GeometryDTO that = ( GeometryDTO )obj;
+      return Object.Equals( geomClass, that.geomClass ) && Object.Equals( srsId, that.srsId ) 
+                                                    && Object.Equals( geoJSON, that.geoJSON );
+    }
+
+    public override int GetHashCode()
+    {
+      return ( geomClass, srsId, geoJSON ).GetHashCode();
+    }
+  }
+}
