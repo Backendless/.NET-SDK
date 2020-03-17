dc29f3dee3732535e93dabb7743e0e224eda711f
Nikita <nikita@themidnightcoders.com>
2020-03-17 19:33:41 +0200

    Geometry

--- Backendless/Persistence/SpatialReferenceSystemEnum.cs
+++ Backendless/Persistence/SpatialReferenceSystemEnum.cs
@@ -8,20 +8,32 @@ namespace BackendlessAPI
 {
   public class SpatialReferenceSystemEnum
   {
+    /*
     public IList<SpatialReferenceSystemEnum> values = new List<SpatialReferenceSystemEnum>(); 
-    static SpatialReferenceSystemEnum CARTESIAN = new SpatialReferenceSystemEnum( 0, "Cartesian" ));
+    static SpatialReferenceSystemEnum CARTESIAN = new SpatialReferenceSystemEnum( 0, "Cartesian" );
     static SpatialReferenceSystemEnum PULKOVO_1995 = new SpatialReferenceSystemEnum( 4200, "Pulkovo_1995" );
     static SpatialReferenceSystemEnum WGS84 = new SpatialReferenceSystemEnum( 4326, "WGS 84" );
     static SpatialReferenceSystemEnum WGS84_PSEUDO_MERCATOR = new SpatialReferenceSystemEnum( 3857, "WGS 84 / Pseudo-Mercator" );
     static SpatialReferenceSystemEnum WGS84_WORLD_MERCATOR = new SpatialReferenceSystemEnum( 3395, "WGS 84 / World Mercator" );
+    */
+    public enum ReferenceSystemEnum
+    {
+      CARTESIAN = 0,
+      PULKOVO_1995 = 4200,
+      WGS84 = 4326,
+      WGS84_PSEUDO_MERCATOR = 3857,
+      WGS84_WORLD_MECATOR = 3395
+    }
+    Dictionary<string, object> values = new Dictionary<string, object>();
+
 
     
 
     private int SrsId { get; }
     private String Name { get; }
     
-    public static SpatialReferenceSystemEnum SERVER_DEFAULT = CARTESIAN;
-    public static SpatialReferenceSystemEnum DEFAULT = SpatialReferenceSystemEnum.WGS84;
+    //public static ReferenceSystemEnum SERVER_DEFAULT = ReferenceSystemEnum.CARTESIAN;
+    //public static ReferenceSystemEnum DEFAULT = ReferenceSystemEnum.WGS84;
 
     public static SpatialReferenceSystemEnum valueBySRSId( IList<SpatialReferenceSystemEnum> values, int srsId)
     {
