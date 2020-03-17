3615f954f3d1f824337f27829ce3277e3b0ceb6a
Nikita <nikita@themidnightcoders.com>
2020-03-17 23:08:50 +0200

    Updated SpatialReferenceSystemEnum

--- Backendless/Persistence/SpatialReferenceSystemEnum.cs
+++ Backendless/Persistence/SpatialReferenceSystemEnum.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +7,6 @@ namespace BackendlessAPI
 {
   public class SpatialReferenceSystemEnum
   {
-    /*
-    public IList<SpatialReferenceSystemEnum> values = new List<SpatialReferenceSystemEnum>(); 
-    static SpatialReferenceSystemEnum CARTESIAN = new SpatialReferenceSystemEnum( 0, "Cartesian" );
-    static SpatialReferenceSystemEnum PULKOVO_1995 = new SpatialReferenceSystemEnum( 4200, "Pulkovo_1995" );
-    static SpatialReferenceSystemEnum WGS84 = new SpatialReferenceSystemEnum( 4326, "WGS 84" );
-    static SpatialReferenceSystemEnum WGS84_PSEUDO_MERCATOR = new SpatialReferenceSystemEnum( 3857, "WGS 84 / Pseudo-Mercator" );
-    static SpatialReferenceSystemEnum WGS84_WORLD_MERCATOR = new SpatialReferenceSystemEnum( 3395, "WGS 84 / World Mercator" );
-    */
     public enum ReferenceSystemEnum
     {
       CARTESIAN = 0,
@@ -24,38 +15,22 @@ namespace BackendlessAPI
       WGS84_PSEUDO_MERCATOR = 3857,
       WGS84_WORLD_MECATOR = 3395
     }
-    Dictionary<string, object> values = new Dictionary<string, object>();
 
+    public static ReferenceSystemEnum SERVER_DEFAULT = ReferenceSystemEnum.CARTESIAN;
+    public static ReferenceSystemEnum DEFAULT = ReferenceSystemEnum.WGS84;
 
-    
-
-    private int SrsId { get; }
-    private String Name { get; }
-    
-    //public static ReferenceSystemEnum SERVER_DEFAULT = ReferenceSystemEnum.CARTESIAN;
-    //public static ReferenceSystemEnum DEFAULT = ReferenceSystemEnum.WGS84;
-
-    public static SpatialReferenceSystemEnum valueBySRSId( IList<SpatialReferenceSystemEnum> values, int srsId)
+    static Dictionary<object, String> values = new Dictionary<object, String>(){ { 0, "Cartesian"},{ 4200, "Pulkovo_1995"},
+                                   { 4326, "WGS84" },{3867,"WGS 84 / Pseudo-Mercator"}, {3395, "WGS 84 / World Mercator"}};
+    public static String valueBySRSId( int srsId )
     {
-      foreach ( SpatialReferenceSystemEnum srs in values )
-      {
-        if ( srs.SrsId == srsId )
-          return srs;
-      }
-
-      throw new ArgumentException( $"SpatialReferenceSystem doesn't contain value with id {srsId}" );
-    }
-
-    internal SpatialReferenceSystemEnum( int srsId, String name )
-    {
-      SrsId = srsId;
-      Name = name;
-      values.Add( this );
+      if ( values["srsId"] != null )
+        return values["srsId"]; 
+      throw new ArgumentException( $"SpatialReferenceSystem doesn't contain value with id {values.Keys}" );
     }
 
     public override String ToString()
     {
-      return $"{Name}({SrsId})";
+      return $"{values.Values}({values.Keys})";
     }
   }
 }
