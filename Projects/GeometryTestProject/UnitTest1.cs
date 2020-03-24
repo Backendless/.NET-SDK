f2f7cbc07dbdca90e5a4e3a65345562e0cd50315
Nikita <nikita@themidnightcoders.com>
2020-03-24 19:29:09 +0200

    Update UnitTest1.cs

--- Projects/GeometryTestProject/UnitTest1.cs
+++ Projects/GeometryTestProject/UnitTest1.cs
@@ -37,15 +37,16 @@ namespace GeometryTestProject
       list.Add( new Point().SetX( 3.05 ).SetY( 8.6 ) );
       list.Add( new Point().SetX(2.04).SetY(11.006) );
 
+      LineString finalLine = new LineString( list );
       LineString line = ( LineString )geometry;
 
-      CollectionAssert.AreEqual( line.GetPoints(), list , "Points was not Equal" );
+      Assert.AreEqual( line, finalLine , "Points was not Equal" );
     }
     [TestMethod]
     public void TestMethodPolygon()
     {
       WKTParser wkt = new WKTParser();
-      Geometry geometry = wkt.Read( "POLYGON((1.01 1.02, 2.01 2.02, 3.01 3.02, 1.05 2.02), (1.012 1.015, 1.5 1.03, 1.05 1.06))" );
+      Geometry geometry = wkt.Read( "POLYGON((1.01 1.02, 2.01 2.02, 3.01 3.02, 1.05 2.02), (1.013 1.014, 1.015 1.016))" );
       Type t = typeof( Polygon );
       Assert.IsInstanceOfType( geometry, t, "Type is not equal \"Polygon\"" );
 
@@ -55,9 +56,20 @@ namespace GeometryTestProject
       tempList.Add( new Point().SetX( 3.01 ).SetY( 3.02 ) );
       tempList.Add( new Point().SetX( 1.05 ).SetY( 2.02 ) );
 
+      List<Point> tempList2 = new List<Point>();
+
+      tempList2.Add( new Point().SetX( 1.013 ).SetY( 1.014 ) );
+      tempList2.Add( new Point().SetX( 1.015 ).SetY( 1.016 ) );
+      LineString tempLines = new LineString( tempList2 );
+      List<LineString> lines = new List<LineString>();
+      lines.Add( tempLines );
+
+
+      Polygon poly = new Polygon( tempList, lines );
+
       Polygon polygon = ( Polygon )geometry;
 
-      CollectionAssert.AreEqual( polygon.GetBoundary().GetPoints(), tempList, "Object 'boundary' from \"Polygon\" is not equal actual object" );
+      Assert.AreEqual( polygon, poly, "Object \"Polygon\" is not equal actual object" );
     }
     [TestMethod]
     public void TestJSONPointMethod()
@@ -89,9 +101,10 @@ namespace GeometryTestProject
       list.Add( new Point().SetX( 3.01 ).SetY( 3.02 ) );
       list.Add( new Point().SetX( 4.01 ).SetY( 4.02 ) );
 
+      LineString listLineStr = new LineString( list );
       LineString line = ( LineString )geometry;
 
-      CollectionAssert.AreEqual( line.GetPoints(), list, "Points was not Equal" );
+      Assert.AreEqual( line, listLineStr, "Points was not Equal" );
     }
     [TestMethod]
     public void TestJSONPolygonMethod()
@@ -108,8 +121,6 @@ namespace GeometryTestProject
       tempList.Add( new Point().SetX( 2.02 ).SetY( 2.03 ) );
       tempList.Add( new Point().SetX( 1.015 ).SetY( 1.017 ) );
 
-      LineString boundary = new LineString( tempList );
-
       List<Point> tempList2 = new List<Point>();
 
       tempList2.Add( new Point().SetX( 1.013 ).SetY( 1.014 ) );
@@ -124,7 +135,7 @@ namespace GeometryTestProject
       Polygon polygon = ( Polygon )geometry;
 
       bool y = polygon.Equals( poly );
-     // Assert.AreEqual( polygon, poly, "Object \"Polygon\" is not equal actual object" );
+      Assert.AreEqual( polygon, poly, "Object \"Polygon\" is not equal actual object" );
     }
   }
 }
