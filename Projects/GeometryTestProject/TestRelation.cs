05fdb8d362943f58af3eda72e863fd4302b86615
Nikita <nikita@themidnightcoders.com>
2020-04-22 22:17:27 +0300

    Updated tests

--- Projects/GeometryTestProject/TestRelation.cs
+++ Projects/GeometryTestProject/TestRelation.cs
@@ -37,34 +37,42 @@ namespace GeometryTestProject
     }
 
     [TestMethod]
-    public void TestRelationsPageSize()
+    public void TestRelationDepthZero()
     {
       DataQueryBuilder qb = DataQueryBuilder.Create();
       qb.AddAllProperties();
-      qb.SetRelationsPageSize( 15);
+      qb.SetRelationsDepth( 0 );
+      qb.SetRelated( new List<String> { "Related" } );
       IList<Dictionary<String, Object>> res = Backendless.Data.Of( "Order" ).Find( qb );
+
+      Assert.IsFalse( !res[ 0 ].ContainsKey( "Related" ) );
+      Assert.IsFalse( !res[ 1 ].ContainsKey( "Related" ) );
     }
 
     [TestMethod]
-    public void TestRelationDepthNULL()
+    public void TestRelationsPageSizeOne()
     {
       DataQueryBuilder qb = DataQueryBuilder.Create();
       qb.AddAllProperties();
-      qb.SetRelationsDepth( null );
+      qb.SetRelationsPageSize( 1 );
+      qb.SetRelated( new List<String> { "Related" } );
       IList<Dictionary<String, Object>> res = Backendless.Data.Of( "Order" ).Find( qb );
+      Object[] rel = (Object[]) res[ 0 ][ "Related" ];
 
-      Assert.IsFalse( res[ 0 ].ContainsKey( "Related" ) );
-      Assert.IsFalse( res[ 1 ].ContainsKey( "Related" ) );
+      Assert.IsTrue( rel.Length == 1 );
     }
 
     [TestMethod]
-    public void TestRelationPageSizeNULL()
+    public void TestRelationPageSizeZero()
     {
       DataQueryBuilder qb = DataQueryBuilder.Create();
       qb.AddAllProperties();
       qb.SetRelationsPageSize( 0 );
       qb.SetRelated( new List<String> { "Related" } );
       IList<Dictionary<String, Object>> res = Backendless.Data.Of( "Order" ).Find( qb );
+      Object[] rel = (Object[]) res[ 0 ][ "Related" ];
+
+      Assert.IsTrue( rel.Length == 0 );
     }
 
     [TestMethod]
@@ -74,6 +82,7 @@ namespace GeometryTestProject
       qb.AddAllProperties();
       qb.SetRelated( new List<String> { "Related" } );
       IList<Order> res = Backendless.Data.Of<Order>().Find( qb );
+
       Assert.IsTrue( res[0].Related.Count == 1 );
     }
 
@@ -85,6 +94,7 @@ namespace GeometryTestProject
       qb.SetRelated( new List<String> { "Related" } );
       qb.SetSortBy( new List<String> { "age" } );
       IList<Dictionary<String, Object>> res = Backendless.Data.Of( "Order" ).Find( qb );
+
       Assert.IsTrue( (Double) res[ 0 ][ "age" ] == 5.0 && (Double) res[1]["age"] == 10.0 );
     }
   }
