5716b3c19e24730795185282931a223e02153db2
Nikita <nikita@themidnightcoders.com>
2020-06-18 12:15:33 +0300

    api keys have been removed

--- Projects/TestProject/TestInitialization.cs
+++ Projects/TestProject/TestInitialization.cs
@@ -11,12 +11,12 @@ namespace TestProject
   public class TestInitialization
   {
 #if DEV_TEST
-      public const String APP_API_KEY = "F8F82BF0-414F-36CB-FFE1-303FA538ED00";
-      private const String DOTNET_API_KEY = "AB320716-358B-4BBF-AF9D-4F4B98F03363";
+      public const String APP_API_KEY = "";
+      private const String DOTNET_API_KEY = "";
       private const String BKNDLSS_URL = "http://apitest.backendless.com";
 #else
-      public const String APP_API_KEY = "CF7398E8-7F3D-64F9-FF3B-FB3CE8893200";
-      private const String DOTNET_API_KEY = "9808682C-32C2-EC87-FFF5-CAB709BE2400";
+      public const String APP_API_KEY = "";
+      private const String DOTNET_API_KEY = "";
       private const String BKNDLSS_URL = "http://api.backendless.com";
 #endif
 
