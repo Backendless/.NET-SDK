cabb2ae0ca15297ef4943c6991214dde4b6679cc
Nikita Fedorishchev <nikita@themidnightcoders.com>
2023-08-08 15:44:40 +0300

    BackendlessExpression implementation

---
+++ Backendless/Persistence/BackendlessExpression.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Weborb.Service;
+
+namespace BackendlessAPI
+{
+
+  public class BackendlessExpression
+  {
+    [SetClientClassMemberName("value")]
+    public string Value { get; set; }
+
+    public BackendlessExpression() { }
+
+    public BackendlessExpression(String value)
+    {
+      Value = value;
+    }
+  }
+}
