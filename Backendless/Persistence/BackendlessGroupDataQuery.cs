e622540234cbd3ff128d86bc9dd1ce923842f845
Nikita Fedorishchev <nikita@themidnightcoders.com>
2024-02-19 13:47:19 +0200

    * GroupedData.cs: * GroupResult.cs: * GroupingColumnValue.cs: * GroupDataQueryBuilder.cs: * BackendlessGroupDataQuery.cs: * PagedGroupQueryBuilder.cs: group api

---
+++ Backendless/Persistence/BackendlessGroupDataQuery.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using BackendlessAPI.Data;
+using Weborb.Service;
+
+namespace BackendlessAPI.Persistence
+{
+  public class BackendlessGroupDataQuery : BackendlessDataQuery
+  {
+    private const Int32 GROUP_DEPTH = 3;
+
+    [SetClientClassMemberName("groupPageSize")]
+    public Int32 GroupPageSize { get; set; } = DEFAULT_PAGE_SIZE;
+
+    [SetClientClassMemberName("RecordsPageSize")]
+    public Int32 RecordsPageSize { get; set; } = DEFAULT_PAGE_SIZE;
+
+    [SetClientClassMemberName("groupDepth")]
+    public Int32 GroupDepth { get; set; } = GROUP_DEPTH;
+
+    [SetClientClassMemberName("groupPath")]
+    public List<GroupingColumnValue> GroupPath { get; set; }
+
+    public BackendlessGroupDataQuery() : base() { }
+  }
+}
---
+++ Backendless/Persistence/GroupResult.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using BackendlessAPI.Exception;
+
+namespace BackendlessAPI.Persistence
+{
+  public class GroupResult
+  {
+    private Boolean hasNextPage = false;
+    private List<GroupedData> items = new List<GroupedData>();
+
+
+
+    public Boolean IsGroups()
+    {
+      if (items.Count == 0)
+      {
+        return false;
+      }
+
+      return true;
+    }
+
+    public List<GroupedData> GetGroupedData()
+    {
+      if(!IsGroups())
+      {
+        throw new BackendlessException("List of items doesn't contain instances of GroupedData."
+                                         + " Use GroupResult#GetPlainItems to access data.");
+      }
+
+      return items;
+    }
+
+    public List<GroupedData> GetPlainItems()
+    {
+      if (IsGroups())
+      {
+        throw new BackendlessException("List of items contains instances of GroupedData. " +
+          "Use GroupResult#GetGroupedData to access data.");
+
+      }
+
+      return items;
+    }
+  }
+}
